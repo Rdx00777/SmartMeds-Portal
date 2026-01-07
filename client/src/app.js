14ef5806636ab9a51470c11eaf2f2d04f4493659
Rdx00777 <167740815+Rdx00777@users.noreply.github.com>
2026-01-07 13:20:36 +0530

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DoctorDashboard from './pages/DoctorDashboard';
+import PatientPortal from './pages/PatientPortal';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<DoctorDashboard />} />
+        <Route path="/patient" element={<PatientPortal />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
